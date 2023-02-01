27706f99596b4d9ab00739d27d4d284d5249098c
PauInsa18 <pouinsadel11@gmail.com>
2023-02-01 20:12:27 +0100

    Practica A Pau

---
+++ Practica A Pau/Exercici2_A.py
@@ -0,0 +1,8 @@
+myDict = {'prueba1':11, 'prueba2':12,'prueba3':13,'prueba4':14,'prueba5':15,'prueba6':16,}
+#Mostrar la longitud de nuestro diccionario
+print(len(myDict))
+#Mostrar SOLO los valores de nuestro diccionario
+print(myDict.values())
+#Eliminar el ultimo item del diccionario
+print(myDict.popitem())
+print(myDict)
